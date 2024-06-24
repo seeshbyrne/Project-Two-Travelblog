00c4be4672dd4fa71d42b4c50b86bad8e0028e8a
saoirse byrne <saoirsebyrne98@gmail.com>
2024-06-24 21:51:40 +0930

    Beginning to add styling

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const usersController = require('./controllers/users.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
-// const path = require('path');
+const path = require('path');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -27,7 +27,7 @@ mongoose.connection.on('connected', () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
